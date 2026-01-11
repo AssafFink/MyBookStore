83622a919ef3cf98f22049b717bcb24a100cdaad
AssafFink <assaf.fink@gmail.com>
2026-01-11 15:50:02 +0200

    Add VAT calculation

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculateVat() {
+    const priceBox = document.getElementById("priceBox");
+    const spanResult = document.getElementById("spanResult");
+    const price = +priceBox.value
+    const vat = price * 0.18;
+    spanResult.innerText = `Price: ${price}, VAT: ${vat}`;
+}
